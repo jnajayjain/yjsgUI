1012d30695651e01ec12dd55048b446281ef4eae
jnajayjain <ajay.jain@pepcus.com>
2018-02-25 19:29:28 +0530

    Ajay - Modified Sequence for bus stop.

--- src/components/StudentRegistrationForm.jsx
+++ src/components/StudentRegistrationForm.jsx
@@ -49,117 +49,49 @@ const studiesArray = [
   }
 ];
 const busStops = [
-  {text: 'बजरंग/नंदा नगर मेन रोड (मंदिर)',
-    value: 'Bajrang/Nanda Nagar Main Road(Mandir)'
-  },
-  {text: 'सुखलिया MR10 मंदिर गली',
-    value: 'Sukhliya MR10 Mandir gali'},
-  {text: 'क्लर्क कॉलोनी  ITI मेन रोड',
-    value: 'Clerk Colony ITI Main Road'
-  },
-  {text: 'गोयल नगर मंदिर',
-    value: 'Goyal Nagar Mandir'
-  },
-  {text: 'ब्रजेशवरी मंदिर IDA 140 JMB',
-    value: 'Brajeshwari Mandir IDA 140 JMB'
-  },
-  {text: 'मिलन हाईट्स',
-    value: 'Milan Heights'
-  },
-  {text: 'वैभव नगर (शर्मा  स्वीट्स)',
-    value: 'Vaibhav Nagar(Sharma Sweets)'
-  },
-  {text: 'सुख शांति/उदय नगर मंदिर',
-    value: 'Sukh Shanti/Uday Nagar Mandir'
-  },
-  {text: 'गोयल नगर बंगाली चौराहा',
-    value: 'Goyal Nagar Bengali Square'
-  },
-  {text: 'तिलक नगर मंदिर',
-    value: 'Tilak Nagar Mandir'
-  },
-  {text: 'कनाड़िया रोड /पत्रकार चौराहा',
-    value: 'Kanadiya Road/ Patrakar Square'
-  },
-  {text: 'पलासिया सर्किल',
-    value: 'Palasia Circle'
-  },
-  {text: 'गीता भवन',
-    value: 'Geeta Bhawan'
-  },
-  {text: 'तिलक नगर  मंदिर',
-    value: 'Tilak Nagar Mandir'
-  },
-  {text: 'जावरवाला मंदिर',
-    value: 'Jaavarwala Mandir'
-  },
-  {text: 'समवशरण मंदिर',
-    value: 'Samwasharan Mandir'
-  },
-  {text: 'विजय नगर-कृष्णा मिल्क सेंटर',
-    value: 'Vijay Nagar - Krishna Milk centre'
-  },
-  {text: 'तुलसी नगर मेन रोड (महालक्ष्मी मंदिर)',
-    value: 'Tulsi Nagar Main Road(Mahalaxmi Mandir)'
-  },
-  {text: 'सत्य साई चौराहा (पांच बालयति मदिर)',
-    value: 'Satya Sai Square(Paanch Baalyati Mandir)'
-  },
-  {text: '78 Scheme (मंदिर)',
-    value: '78 Scheme (Mandir)'
-  },
-  {text: 'कालानी मंदिर गली मेन रोड',
-    value: 'Kaalani Mandir Gali Main Road'
-  },
-  {text: 'साधना नगर मंदिर गली मेन रोड',
-    value: 'Saadhna Nagar Mandir Gali Main Road'
-  },
-  {text: 'गाँधी नगर मंदिर',
+ {text: 'गाँधी नगर मंदिर',
     value: 'Gandhi Nagar Mandir'
   },
-  {text: 'कालानी नगर मंदिर गली मेन रोड - 2',
+{text: 'कालानी मंदिर गली मेन रोड',
+    value: 'Kaalani Mandir Gali Main Road'
+  },
+{text: 'कालानी नगर मंदिर गली मेन रोड - 2',
     value: 'Kaalani Nagar Mandir Gali Main Road - 2'
   },
-  {text: 'अंजनी नगर (60 फुट रोड)',
+{text: 'साधना नगर मंदिर गली मेन रोड',
+    value: 'Saadhna Nagar Mandir Gali Main Road'
+  },
+{text: 'अंजनी नगर (60 फुट रोड)',
     value: 'Anjani Nagar(60 Foot Road)'
   },
-  {text: 'पल्हर नगर बांगड़दा रोड (पानी की टंकी)',
+{text: 'स्मृति नगर मंदिर गली (बांगड़दा रोड)',
+    value: 'Smrti Nagar Mandir Gali(Baangarda Road)'
+  },
+{text: 'पल्हर नगर बांगड़दा रोड (पानी की टंकी)',
     value: 'Palhar Nagar Baangarda Road(Paani ki tanki)'
   },
-  {text: 'रामचंद नगर सर्किल (एरोड्रम रोड)',
+{text: 'इंदौर तार फैक्टरी (संगम नगर)',
+    value: 'Indore Taar Factory(Sangam Nagar)'
+  },
+{text: 'रामचंद नगर सर्किल (एरोड्रम रोड)',
     value: 'Raamchand Nagar Circle (Aerodrom Road)'
   },
-  {text: 'छात्रपति नगर मेन रोड (महावीर बाग)',
+{text: 'छात्रपति नगर मेन रोड (महावीर बाग)',
     value: 'Chhatrapati Nagar Main Road(Mahaveer Bagh)'
   },
-  {text: 'जैन कोलोनी (अहिंसा  गेट)',
-    value: 'Jain Colony(Ahinsa Gate)'
-  },
-  {text: 'सपना संगीता टॉवर सर्किल',
-    value: 'Sapna Sangeeta Tower Circle'
-  },
-  {text: 'भॅवरकुआ बीसीएम',
-    value: 'Bhanwarkuwa BCM'
-  },
-  {text: 'राजमोहल्ला (OPP चैत्यालय)',
-    value: 'RajMohalla(Opp Chaityalaya)'
-  },
-  {text: 'कांच मंदिर मेन रोड (नरसिंह बाजार)',
-    value: 'Kaanch Mandir Main Road(Narsingh Bazar)'
-  },
-  {text: 'बड़ा गणपति',
+{text: 'बड़ा गणपति',
     value: 'Bada Ganpati'
+  },
+{text: 'राजमोहल्ला (OPP चैत्यालय)',
+    value: 'RajMohalla(Opp Chaityalaya)'
   },
   {text: 'रामशाह मंदिर मेन रोड',
     value: 'Ramshah Mandir Main Road'
   },
-  {text: 'स्मृति नगर मंदिर गली (बांगड़दा रोड)',
-    value: 'Smrti Nagar Mandir Gali(Baangarda Road)'
+{text: 'कांच मंदिर मेन रोड (नरसिंह बाजार)',
+    value: 'Kaanch Mandir Main Road(Narsingh Bazar)'
   },
-  {text: 'इंदौर तार फैक्टरी (संगम नगर)',
-    value: 'Indore Taar Factory(Sangam Nagar)'
-  },
-  {text: 'रामबाग चौराहा (कुसुम टाकीज़)',
+{text: 'रामबाग चौराहा (कुसुम टाकीज़)',
     value: 'Rambagh Square(Kusum Talkies)'
   },
   {text: 'चिकमंगलूर चौराहा',
@@ -168,25 +100,92 @@ const busStops = [
   {text: 'जीएसआईटीएस सर्किल',
     value: 'GSITS circle'
   },
-  {text: 'महावीर गेट (सुदामा नगर )',
-    value: 'Mahavir Gate(Sudama Nagar)'
+{text: 'क्लर्क कॉलोनी  ITI मेन रोड',
+    value: 'Clerk Colony ITI Main Road'
   },
-  {text: 'रंजीत हनुमान (गुमाश्ता नगर)',
+  {text: 'बजरंग/नंदा नगर मेन रोड (मंदिर)',
+    value: 'Bajrang/Nanda Nagar Main Road(Mandir)'
+  },
+  {text: 'सुखलिया MR10 मंदिर गली',
+    value: 'Sukhliya MR10 Mandir gali'
+ },
+  {text: 'विजय नगर-कृष्णा मिल्क सेंटर',
+    value: 'Vijay Nagar - Krishna Milk centre'
+  },
+  {text: '78 Scheme (मंदिर)',
+    value: '78 Scheme (Mandir)'
+  },
+{text: 'सत्य साई चौराहा (पांच बालयति मदिर)',
+    value: 'Satya Sai Square(Paanch Baalyati Mandir)'
+  },
+{text: 'तुलसी नगर मेन रोड (महालक्ष्मी मंदिर)',
+    value: 'Tulsi Nagar Main Road(Mahalaxmi Mandir)'
+  },
+  {text: 'जावरवाला मंदिर',
+    value: 'Jaavarwala Mandir'
+  },
+{text: 'समवशरण मंदिर',
+    value: 'Samwasharan Mandir'
+  },
+{text: 'गीता भवन',
+    value: 'Geeta Bhawan'
+  },
+{text: 'पलासिया सर्किल',
+    value: 'Palasia Circle'
+  },
+{text: 'कनाड़िया रोड /पत्रकार चौराहा',
+    value: 'Kanadiya Road/ Patrakar Square'
+  },
+{text: 'तिलक नगर  मंदिर',
+    value: 'Tilak Nagar Mandir'
+  },
+{text: 'गोयल नगर मंदिर',
+    value: 'Goyal Nagar Mandir'
+  },
+  {text: 'गोयल नगर बंगाली चौराहा',
+    value: 'Goyal Nagar Bengali Square'
+  },
+  {text: 'सुख शांति/उदय नगर मंदिर',
+    value: 'Sukh Shanti/Uday Nagar Mandir'
+  },
+{text: 'वैभव नगर (शर्मा  स्वीट्स)',
+    value: 'Vaibhav Nagar(Sharma Sweets)'
+  },
+  {text: 'ब्रजेशवरी मंदिर IDA 140 JMB',
+    value: 'Brajeshwari Mandir IDA 140 JMB'
+  },
+  {text: 'मिलन हाईट्स',
+    value: 'Milan Heights'
+  },
+{text: 'भॅवरकुआ बीसीएम',
+    value: 'Bhanwarkuwa BCM'
+  },
+{text: 'सपना संगीता टॉवर सर्किल',
+    value: 'Sapna Sangeeta Tower Circle'
+  },
+{text: 'जैन कोलोनी (अहिंसा  गेट)',
+    value: 'Jain Colony(Ahinsa Gate)'
+  },
+{text: 'रंजीत हनुमान (गुमाश्ता नगर)',
     value: 'Ranjeet Hanuman(Gumashta Nagar)'
   },
-  {text: 'राजेंद्र नगर D-MART चौराहा',
-    value: 'Rajendra Nagar D-MART Square'
-  },
-  {text: 'वैशाली  नगर गोपुर चौराहा',
-    value: 'Vaishali Nagar Gopur Square'
+  {text: 'महावीर गेट (सुदामा नगर )',
+    value: 'Mahavir Gate(Sudama Nagar)'
   },
   {text: 'परिवहन नगर (नितिन ट्रेडर्स गली)',
     value: 'Parivahan Nagar(Nitin Traders Gali)'
   },
-  {text: 'चोइथराम सर्किल ',
+  {text: 'वैशाली  नगर गोपुर चौराहा',
+    value: 'Vaishali Nagar Gopur Square'
+  },
+{text: 'चोइथराम सर्किल ',
     value: 'Choithram Circle'
+  },
+{text: 'राजेंद्र नगर D-MART चौराहा',
+    value: 'Rajendra Nagar D-MART Square'
   }
 ];
+
 const gender = [
   {
     text: 'पुरुष',
